d6bebecbc7a6db0565b1817bac46053e2b835a62
erblaci <bacul@freemail.hu>
2025-03-27 16:48:52 +0100

    player_movement & tile fix

--- Assets/player_movement.cs
+++ Assets/player_movement.cs
@@ -1,16 +1,81 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D rb;
+    private Vector2 direction = Vector2.right;
+    private Vector2 movement;
+    private float walkspeed = 2f;
+    private int runstate = 1;
+    bool isRunning = false;
+    bool isChangingSpeed = false;
+    private void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        GetInput();
+        MovePlayer();
+    }
+
+    private void GetInput()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        movement = new Vector2(hor, 0f);
+        if (hor<0)
+        {
+            direction = Vector2.left;
+        }else if (hor > 0)
+        {
+            direction = Vector2.right;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+            runstate = 1;
+        }
+
+        if (isRunning&&rb.linearVelocity.x >0&&!isChangingSpeed)
+        {
+            StartCoroutine(ChangeRunstate());
+        }
+    }
+
+    public IEnumerator ChangeRunstate()
+    {
+        isChangingSpeed = true;
+        yield return new WaitForSeconds(2f);
+        if (isRunning&&rb.linearVelocity.x>1&&runstate<4)
+        {
+            Debug.Log(runstate);
+            runstate++;
+        }
+        isChangingSpeed = false;
+    }
+    
+    private void MovePlayer()
+    {
+        if (!isRunning)
+        {
+            rb.AddForce(movement*walkspeed,ForceMode2D.Force);
+        }else if (isRunning)
+        {
+            rb.AddForce(movement*walkspeed*(runstate+1),ForceMode2D.Force);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        Debug.DrawRay(transform.position, direction*1f, Color.red);
     }
 }
