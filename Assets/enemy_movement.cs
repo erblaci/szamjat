c35addce0eb10abba49e412f39c57d4f4664d2b4
WildOver <laubez17@gmail.com>
2025-03-31 04:30:07 +0200

    Enemy és movement
    Megoldottuk az enemy alapjait és mozgását 👍
    
    Co-Authored-By: erblaci <152805816+erblaci@users.noreply.github.com>

---
+++ Assets/enemy_movement.cs
@@ -0,0 +1,41 @@
+//https://www.youtube.com/watch?v=RuvfOl8HhhM
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class enemy_movement : MonoBehaviour
+{
+    public GameObject Apont;
+    public GameObject Bpont;
+    private Rigidbody2D rb;
+    private Transform currentpoint;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentpoint = Bpont.transform;
+        
+    }
+
+    void Update()
+    {
+        Vector2 point = currentpoint.position - transform.position;
+        if (currentpoint == Bpont.transform)
+        {
+            rb.velocity = new Vector2(speed,0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed,0);
+        }
+
+        if (Vector2.Distance(transform.position, currentpoint.position)< 0.5f && currentpoint == Bpont.transform)
+        {
+            currentpoint = Apont.transform;
+        }
+        if (Vector2.Distance(transform.position, currentpoint.position)< 0.5f && currentpoint == Apont.transform)
+        {
+            currentpoint = Bpont.transform;
+        }
+    }
+}
