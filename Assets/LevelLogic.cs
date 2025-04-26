69a3daa15c0644dd125788a79410bd8df202ef33
erblaci <bacul@freemail.hu>
2025-04-26 23:16:11 +0200

    Level logic

---
+++ Assets/LevelLogic.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+public class LevelLogic : MonoBehaviour
+{
+    public float maxTime = 300;
+    public float currentTime;
+    public TMPro.TextMeshProUGUI timerGUI;
+    public bool IsRunning = false;
+    public bool IsEndTriggered = false;
+    public int BabiesFound = 0;
+
+    public void Awake()
+    {
+        currentTime=maxTime;
+        
+    }
+
+    private void Update()
+    {
+        if (!IsRunning)
+        {
+            return;
+        }
+        currentTime -= Time.deltaTime;
+        currentTime = Mathf.Max(currentTime, 0f);
+        UpdateText();
+        if (currentTime==0)
+        {
+            IsRunning = false;
+        }
+    }
+
+    public void UpdateText()
+    {
+       
+        currentTime = Mathf.Max(currentTime, 0f);
+            int minutes = Mathf.FloorToInt(currentTime / 60);
+            int seconds = Mathf.FloorToInt(currentTime % 60);
+            timerGUI.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            
+        
+    }
+    
+}
