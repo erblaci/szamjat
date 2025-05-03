b330e4bfca6ad941843199a9db8db58b0cb3ceb7
erblaci <bacul@freemail.hu>
2025-05-03 15:07:40 +0200

    Pickups

---
+++ Assets/Pickup.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private void Update()
+    {
+        float y=Mathf.Sin(Time.time)/200+transform.position.y;
+        transform.position = new Vector3(transform.position.x,y,transform.position.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        LevelLogic.instance.LevelScore += 100;
+        Destroy(gameObject);
+    }
+}
